0bfc89895300a1698099430d9e6a99fc74bb2f54
RiswinD <riswinfdo@gmail.com>
2026-03-22 11:45:43 +0530

    new

---
+++ problem11.js
@@ -0,0 +1,15 @@
+//Check if a number is positive, negative, or zero. Print the result.
+let a = -5;
+
+if(a > 0)
+{
+    console.log("The number is positive.");
+
+}
+else if(a < 0)
+{
+    console.log("The number is negative.");
+}
+else{
+    console.log("The number is zero.");
+}
