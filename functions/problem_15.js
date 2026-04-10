0e27e0c7528ee3633a6bdf56090970e8bdce0558
RiswinD <riswinfdo@gmail.com>
2026-04-10 09:58:07 +0530

    extra problems

--- functions/problem_15.js
+++ functions/problem_15.js
@@ -1,1 +1,16 @@
 //Write a function that takes an array of strings and returns the longest string.
+function longstr(arr1)
+{
+    let largest=arr1[0];
+    for (let i=1;i<arr1.length;i++)
+    {
+        if (arr1[i].length > largest.length)
+        {
+            largest = arr1[i];
+        }
+    }
+    return largest;
+
+}
+let arr1 =["hello","my","name","is","riswin"];
+console.log(longstr(arr1));
